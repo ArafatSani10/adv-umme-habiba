75890b2b251736437433144ba75e594350555d5a
STechNest <s.technestbd@gmail.com>
2026-08-31 07:11:08 +0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-        
+        <p>© 2026 All Rights Reserved. · Website crafted by STechNest</p>
     </div>
   )
 }
